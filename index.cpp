fe5db87357e58f4fe7392dbaea1f2e0050320ec3
premkumar-4 <154761103+premkumar-4@users.noreply.github.com>
2023-12-25 02:05:00 +0530

    Solved Assignment files are added

---
+++ index.cpp
@@ -0,0 +1,49 @@
+// Question 1
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"Physics"<<endl;
+    cout<<"Wallah";
+    return 0;
+}
+
+// Question 2
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<5+5 <<endl;
+    return 0;
+}
+
+// Question 3
+//Solution : int, bool, float data types take 4, 1, 4 bytes respectively.
+
+// Question 4
+#include<iostream>
+using namespace std;
+int main(){
+    int a = 4;
+    int b = 5;
+    a++, b--;
+    cout<<++a <<" "<<b--;
+    return 0;
+}
+
+// Question 5
+#include<iostream>
+using namespace std;
+    int main() {
+    int r = 10;
+    int pi = 3.14;
+    int circumference = 2*pi*r;
+    cout<<circumference;
+    return 0;
+}
+
+// Question 6
+//Solution :
+//only two of the given options can be correct.
+//2nd option is correct because a variable name can start with underscore and 4th option is correct 
+//because c++ is a case sensitive language ,in this case FLOAT and float are treated differently so FLOAT is not a reserved keyword, so it can be considered as a variable name.
